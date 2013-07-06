7f377cec0e26c44dd96f1a0ed04e4329b46fa192
Nate Aune <natea@jazkarta.com>
2013-07-06 14:26:37 -0400

    don't check in the *.pyc and *.pyo files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url='http://isaacbythewood.com/',
     install_requires=[
         'Django==1.5.1',
-        'mezzanine==1.4.6',
+        'mezzanine==1.4.8',
         'django_compressor==1.3'
     ],
 )
